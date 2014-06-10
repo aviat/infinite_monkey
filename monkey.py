d32e7ae4d8120d4ffba3ef063d19e41469ea2108
Pablo Díaz Ogni <pablo.diazogni@gmail.com>
2014-06-10 16:29:06 -0300

    Created Monkey class. Added argument parser

--- monkey.py
+++ monkey.py
@@ -1,36 +1,50 @@
-import sys
 import random
 import string
-import threading
 import time
+import argparse
+from threading import Thread
 
-lower_letters = string.letters[:26]
-
-word = sys.argv[1].lower()
-lword = len(word)
-range_word = range(len(word))
-threads = int(sys.argv[2])
 stop = False
 
-def generate(range_word):
-    return ''.join([random.choice(lower_letters) for x in range_word])
+class Monkey(Thread):
 
-def find(word, range_word):
-    global stop
-    new = generate(range_word)
-    while new != word and not stop:
-        new = generate(range_word)
-    if new == word:
-        print "Found: %s" % new
-        stop = True
+    def __init__(self, name, word, letters, *args, **kwargs):
+        super(Monkey, self).__init__(*args, **kwargs)
+        self.name = 'Monkey-%s' % (name,)
+        self.letters = letters
+        self.word = word
+        self.range_word = range(len(word))
 
-threads = [threading.Thread(target=find, args=(word,range_word)) for x in range(threads)]
-begin = time.time()
-for t in threads:
-    t.setDaemon(True)
-    t.start()
+    def run(self):
+        global stop
+        new = self.generate()
+        while new != self.word and not stop:
+            new = self.generate()
+        if new == word:
+            print "%s has found: %s" % (self.name, new)
+            stop = True
 
-for t in threads:
-    t.join(1000)
+    def generate(self):
+        return ''.join([random.choice(self.letters) for x in self.range_word])
 
-print "Finished in: %s" % (time.time() - begin,)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--word', type=str, required=True)
+    parser.add_argument('-t', '--threads', type=int, default=1)
+    args = parser.parse_args()
+
+    word = args.word
+    range_word = range(len(word))
+    threads_count = args.threads
+
+    threads = [Monkey(x+1, word, string.lowercase) for x in range(threads_count)]
+    begin = time.time()
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+
+    for t in threads:
+        t.join(1000)
+
+    print "Finished in: %s" % (time.time() - begin,)
